85062c0ea933fa14e1c9b6fbc20b95257207d80a
ojthesixth <110111400+ojthesixth@users.noreply.github.com>
2022-07-29 16:23:38 +0200

    2022-gameproject1v2
    Fixed bug, added debug.log 'bonk'.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,22 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody _rigidbodyp;
     public float _speed = 10f;
-    public GameObject projectile; }
+    public GameObject bullet;
 // public float _rotation;
 
-// Start is called before the first frame update
-
-    void Start() { }
-
-// Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision col)
     {
-    if (Input.GetButtonDown("Jump")) {
-        for (var i = 0; i < 10; i++)
-            object.Instantiate(projectile); }
+        Debug.Log("bonk");
+    }
 
+   void Start() { }
+
+
+ void Update()
+
+    { if (Input.GetButtonDown("Jump")) 
+            for (var i = 0; i < 10; i++) {
+                GameObject o = Instantiate(bullet); }
+    
 
         float v = Input.GetAxis("Vertical") * _speed;
         float r = Input.GetAxis("Horizontal") * _speed;
         _rigidbodyp.velocity = new Vector3(r, 0, v);
-    }
+     }}
