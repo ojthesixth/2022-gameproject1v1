e05afa59c51977d433e9214653e20b168d291e7f
ojthesixth <110111400+ojthesixth@users.noreply.github.com>
2022-09-01 16:23:24 +0200

    2022-gameproject1v4
    fixed script.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,18 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody _rigidbodyp;
     public float _speed = 10f;
-    public Transform bulletfile;
-    public GameObject _bulletPrefab;
+    public GameObject bulletfile;
+   // public GameObject _bulletPrefab;
     public GameObject _bullet;
-// public float _rotation;
+    // public float _rotation;
 
-
+    /*
     private void FireBullet()
     {
-        GameObject bullet = Instantiate(_bulletPrefab);
+        GameObject bullet = Instantiate(_bullet);
     }
 
-    /* void Update OnCollisionEnter(Collision col)
+     void Update OnCollisionEnter(Collision col)
      {
          Debug.Log("bonk");
      }
@@ -26,15 +26,18 @@ public class PlayerMovement : MonoBehaviour
     void Start() { }
 
 
- void Update()
+    void Update()
 
-    { if (Input.GetButtonDown("Jump"))
-        { FireBullet(); }
-            for (var i = 0; i < 10; i++) {
-                GameObject o = Instantiate(bullet); }
-    
+    {
+        if (Input.GetButtonDown("Jump"))
+      //  { FireBullet(); }
+        for (var i = 0; i < 1; i++)
+        {
+            GameObject o = Instantiate(_bullet, bulletfile.transform.position, _bullet.transform.localRotation );
+        }
 
         float v = Input.GetAxis("Vertical") * _speed;
         float r = Input.GetAxis("Horizontal") * _speed;
         _rigidbodyp.velocity = new Vector3(r, 0, v);
-     }} 
+    }
+}
